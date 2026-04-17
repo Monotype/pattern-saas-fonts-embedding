1f9c86897a0aec5fd4bb84e16ec80cbafcfdfe08
Jeff Carey <jeff.carey@monotype.com>
2026-04-17 08:40:09 -0700

    working

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
-import path from "path";
 import express from "express";
+import path from "path";
 
 const app = express();
 
+const fontPath = path.resolve("fonts/MyFont.woff2");
+
 app.get("/fonts/myfont", (req, res) => {
-  res.sendFile(path.resolve("fonts/MyFont.woff2"));
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.sendFile(fontPath);
 });
 
 app.listen(3000, () => {
