611beda1c192b2c92bff3f887a9753050a400d2b
jeffreyrcarey <jeff.carey@monotype.com>
2026-04-17 09:46:50 -0700

    feat: add PR #6 full runnable implementation (#3)
    * feat: add PR #6 full runnable implementation
    
    * chore: fix module type and add start script for SaaS pattern server
    
    * not getting font
    
    * working
    
    * explain

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log("SaaS font server placeholder")
+import express from "express";
+import path from "path";
+
+const app = express();
+
+const fontPath = path.resolve("fonts/MyFont.woff2");
+
+app.get("/fonts/myfont", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.sendFile(fontPath);
+});
+
+app.listen(3000, () => {
+  console.log("Font server running on http://localhost:3000");
+});
