b2577e8d7ea7f92b0ed8f3a6d8802b666530ac18
Jeff Carey <jeff.carey@monotype.com>
2026-04-17 08:19:38 -0700

    not getting font

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-import express from "express"; const app=express(); app.get("/font",(req,res)=>{res.sendFile("/fonts/MyFont.woff2");}); app.listen(3000);
+import path from "path";
+import express from "express";
+
+const app = express();
+
+app.get("/fonts/myfont", (req, res) => {
+  res.sendFile(path.resolve("fonts/MyFont.woff2"));
+});
+
+app.listen(3000, () => {
+  console.log("Font server running on http://localhost:3000");
+});
